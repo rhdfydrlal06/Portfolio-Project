5ffdeee984f5d8244ad645db149a9af9b0eb3d11
[2기-1반]김희재 <gmlwo0109@gmail.com>
2021-08-28 01:45:01 +0900

    서버 주소 다시다시 .. 변경

--- frontend/src/Env.js
+++ frontend/src/Env.js
@@ -1,1 +1,1 @@
-export const BACKEND_URL = "http://http://kdt-1st-project-75.koreacentral.cloudapp.azure.com/api"
+export const BACKEND_URL = "http://kdt-1st-project-75.koreacentral.cloudapp.azure.com/api"
